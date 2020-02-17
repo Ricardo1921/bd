5d94167cbe7cabbdd486d0e0bd1222860365d50c
Ricardo1921 <49916582+Ricardo1921@users.noreply.github.com>
2020-02-17 16:28:14 +0000

    Add files via upload

---
+++ bd/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace bd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RichTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richTextBox1.Text = File.ReadAllText(@"C:\Users\noddy\source\repos\bd\bd\Resources\9.txt");
+        }
+    }
+}
